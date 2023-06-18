d8c95c789bc3522d935c3c5e66415b5c43418148
hitoshiQQ <127374716+hitoshiQQ@users.noreply.github.com>
2023-06-18 17:02:27 +0600

    Add files via upload

---
+++ Task 3/3. Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,17 @@
+﻿class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.size();
+        vector<int> position(128, -1);
+        int res = 0;
+
+        for (int j = 0, i = 0; i < n; i++) {
+            if (position[s[i]] != -1) {
+                j = max(j, position[s[i]] + 1);
+            }
+            res = max(res, i - j + 1);
+            position[s[i]] = i;
+        }
+        return res;
+    }
+};
