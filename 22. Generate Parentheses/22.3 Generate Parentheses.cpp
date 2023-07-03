d8af7cad5c74333884f3b1e80df7b36d5fb89fbf
hitoshiQQ <127374716+hitoshiQQ@users.noreply.github.com>
2023-07-03 16:11:20 +0600

    Update 22.3 Generate Parentheses.cpp

--- 22. Generate Parentheses/22.3 Generate Parentheses.cpp
+++ 22. Generate Parentheses/22.3 Generate Parentheses.cpp
@@ -1,3 +1,4 @@
+// фcceleration
 const int ZERO = [] {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -10,19 +11,22 @@ class Solution {
             st.push_back(string(current.begin(), current.end()));
             return;
         }
+        
+        // the number of how many brackets to open
         if (open > 0) {
             current.push_back('(');
             generateParenthesis(current, open - 1, close + 1, st);
             current.pop_back();
-
         }
+        
+        // the number of how many parentheses to close
         if (close > 0) {
             current.push_back(')');
             generateParenthesis(current, open, close - 1, st);
             current.pop_back();
         }
     }
-
+     
     double factorial(int n) {
         if (n == 0) {
             return 1;
@@ -36,6 +40,8 @@ public:
     vector<string> generateParenthesis(int n) {
         int st_size = round(factorial(2 * n) / factorial(n) / factorial(n) / (n + 1));
         vector<string> st;
+        
+        // acceleration due to memory redundancy
         st.reserve(st_size);
         vector<char> current;
         generateParenthesis(current, n, 0, st);
