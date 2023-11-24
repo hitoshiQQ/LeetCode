3a4b0405ed1ed463f06a28def02b3fda52791928
hitoshiQQ <antr10203040@gmail.com>
2023-11-24 09:03:25 +0600

    Add files via upload

---
+++ 96. Unique Binary Search Trees/96. Unique Binary Search Trees.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> T(n + 1, 0);
+
+        T[0] = T[1] = 1;
+
+        for (int r = 2; r <= n; r++) {
+            for (int mid = 1; mid <= r; mid++) {
+
+                /*
+                if the tree is rooted at mid,
+                the left and right subtree would be:
+                [1...mid-1] and [mid+1...r]
+                since the right subtree's max size is r-1,
+                so mid starts from 1,
+                and since the left subtree's max size is r-1,
+                so mid ends at r
+                */
+
+                T[r] += T[mid - 1] * T[r - mid];
+            }
+        }
+        return T[n];
+    }
+};
