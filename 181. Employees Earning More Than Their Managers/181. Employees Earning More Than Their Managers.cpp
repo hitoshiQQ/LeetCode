70bfe08a86d392d08be56da2259f06f84d203bf4
hitoshiQQ <antr10203040@gmail.com>
2024-04-01 08:35:23 +0600

    Add files via upload

---
+++ 181. Employees Earning More Than Their Managers/181. Employees Earning More Than Their Managers.cpp
@@ -0,0 +1,7 @@
+
+# Write your MySQL query statement below
+SELECT Worker.name AS Employee
+FROM Employee AS Worker
+INNER JOIN EMployee AS Manager
+ON(Worker.managerId = Manager.id)
+WHERE Worker.salary > Manager.salary;
